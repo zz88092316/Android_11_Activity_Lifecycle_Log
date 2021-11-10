b07b12ce884eb5199a37730631cd0ca66b39ea05
zz88092316 <zz88092316@gmail.com>
2021-11-10 11:34:39 +0800

    Initial commit

---
+++ app/src/main/java/com/skypan/android_11_activity_lifecycle_log/MainActivity2.java
@@ -0,0 +1,60 @@
+package com.skypan.android_11_activity_lifecycle_log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Log.d("tag", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("tag", "onStart");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("tag", "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("tag", "onRestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("tag", "onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("tag", "onDestroy");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("tag", "onStop");
+    }
+
+    public void Switch(View view) {
+        Intent intent = new Intent(MainActivity2.this,MainActivity.class);
+        startActivity(intent);
+    }
+}
